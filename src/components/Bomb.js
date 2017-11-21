20bf9972472d594b1899e4c06f2f4c1e9ee47375
rebeccanewborn <rjn58@cornell.edu>
2017-11-21 09:49:17 -0500

    Done.

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,18 @@
 // Bomb Component Code Goes Here
+import React from "react";
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+  render() {
+    return this.state.secondsLeft === 0
+      ? "Boom!"
+      : `${this.state.secondsLeft} seconds left before I go boom!`;
+  }
+}
+
+export default Bomb;
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,16 @@
 // Bomb Component Code Goes Here
+import React from "react";
+class ImageSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSlideIndex: 0
+    };
+  }
+
+  render() {
+    return `I am on slide ${this.state.currentSlideIndex}`;
+  }
+}
+
+export default ImageSlider;
